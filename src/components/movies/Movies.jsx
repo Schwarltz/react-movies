64e31828581861880249b2594138e287e4d8b53b
Henry Ho <z5312521@ad.unsw.edu.au>
2021-12-17 17:43:18 +1100

    standardised card layout; added pagination

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Pagination from 'react-bootstrap/Pagination';
 
 import MovieCard from './movie-card/Movie-Card';
 import { getLatest }  from '../../services/latest';
@@ -10,28 +11,48 @@ import './Movies.scss';
 
 
 const Movies = () => {
+    const [pageNum, setPageNum] = useState(1);
+    
     const [movies, setMovies] = useState(null);
-
     useEffect(() => {
         let mounted = true;
-        getLatest(1)
+        getLatest(pageNum)
           .then(items => {
             if(mounted) {
               setMovies(items)
             }
           })
         return () => mounted = false;
-    })
+    }) 
+
+    let items = [];
+
+    if (movies) {
+      for (let number = Math.max(pageNum-3,1); number <= Math.min(pageNum+3, movies.total_pages); number++) {
+        items.push(
+          <Pagination.Item key={number} active={number === pageNum} onClick={()=>setPageNum(number)}>
+            {number}
+          </Pagination.Item>,
+        );
+      }
+    }
+    
 
     return <>
-        <h1>Latest Movies</h1>
         <Container bsPrefix="moviesGrid">
-          
-            {movies != null ? movies.results.map(movie => <MovieCard key={movie.title} details={movie} />) : <p>Loading</p>}
-          
-          
+            {movies != null ? movies.results.map(movie => <MovieCard key={movie.title} details={movie} />) 
+              : <p>Loading</p>}
         </Container>
         
+        <Container style={{"display":"flex", "align-content" : "center"}}>
+          <Pagination style={{"margin":"auto"}}>
+            <Pagination.First onClick={()=>{setPageNum(1)}}/>
+            <Pagination.Prev onClick={()=>{setPageNum(Math.max(pageNum - 1,1))}}/>
+            {items}
+            <Pagination.Next onClick={()=>{setPageNum(Math.min(pageNum + 1,movies.total_pages))}}/>
+            <Pagination.Last onClick={()=>{setPageNum(movies.total_pages)}}/>
+          </Pagination>
+        </Container>
     </>
 }
 
--- src/components/movies/movie-card/Movie-Card.jsx
+++ src/components/movies/movie-card/Movie-Card.jsx
@@ -1,33 +1,41 @@
 import React, { useState } from 'react';
-import Card from 'react-bootstrap/Card';
+
+import Accordian from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
 import './Movie-Card.scss';
 
 function MovieCard(props) {
     const [clicked, setClicked] = useState(false);
     
-    function handleOnClick() {
-        setClicked(!clicked);
-        console.log('flipping clicked');
+    let voteColour = "vote";
+    if (props.details.vote_average < 5) {
+        voteColour="vote-low";
+    } else if (props.details.vote_average < 8) {
+        voteColour="vote-med";
+    } else {
+        voteColour="vote-high";
     }
 
     return <Card bsPrefix="movieCard">
-    {/* return <Card> */}
-        <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w200${props.details.poster_path}`}/>
+        <Container bsPrefix="container">
+            <Card.Img href={props.details.title} src={`https://image.tmdb.org/t/p/w500${props.details.poster_path}`}/>
+            <Container bsPrefix={voteColour}>
+                <b>{props.details.vote_average}</b>
+            </Container>
+
+            <Container bsPrefix="overlay">
+                {!clicked ? <button className="moreInfo" onClick={()=>{setClicked(true)}}>More Info</button>
+                : <div class="overview">
+                    <p>{props.details.overview}</p>
+                </div>
+                }
+            </Container>
+        </Container>
         <Card.Body>
-            <Card.Title>{props.details.title}</Card.Title>
-            <Button variant="primary" 
-                    size="lg" 
-                    style={{width:"100%"}} 
-                    onClick={()=> {setClicked(!clicked)}}
-                    >
-                {!clicked ? "More Info" : "Less Info"}
-            </Button>
-            {clicked ? 
-                <p>{props.details.overview}</p>: null
-}
+            <Card.Title bsPrefix="movieTitle">{props.details.title}</Card.Title>
         </Card.Body>
-        
     </Card>
 }
 
